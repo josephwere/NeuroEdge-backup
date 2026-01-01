7bbb57332180ebc65828a4be3db67f68d66294f7
josephwere <josephogwe8@gmail.com>
2026-01-02 00:02:11 +0300

    Create neurochain_validator_agent.go

---
+++ kernel/agents/neurochain_validator_agent.go
@@ -0,0 +1,41 @@
+package agents
+
+import (
+	"fmt"
+	"neuroedge/kernel/core"
+)
+
+type NeuroChainValidatorAgent struct {
+	EventBus *core.EventBus
+}
+
+func NewNeuroChainValidatorAgent(bus *core.EventBus) *NeuroChainValidatorAgent {
+	return &NeuroChainValidatorAgent{
+		EventBus: bus,
+	}
+}
+
+func (n *NeuroChainValidatorAgent) Start() {
+	fmt.Println("🚀 NeuroChainValidatorAgent started")
+	ch := make(chan map[string]interface{})
+	n.EventBus.Subscribe("block:new", ch)
+	go func() {
+		for event := range ch {
+			fmt.Println("[NeuroChainValidatorAgent] New block received:", event)
+			n.ValidateBlock(event)
+		}
+	}()
+}
+
+func (n *NeuroChainValidatorAgent) Stop() {
+	fmt.Println("🛑 NeuroChainValidatorAgent stopped")
+}
+
+func (n *NeuroChainValidatorAgent) Name() string {
+	return "NeuroChainValidatorAgent"
+}
+
+// Example: Validate new block
+func (n *NeuroChainValidatorAgent) ValidateBlock(block map[string]interface{}) {
+	fmt.Println("[NeuroChainValidatorAgent] Validating block:", block)
+}
