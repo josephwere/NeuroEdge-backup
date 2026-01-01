4393b826983344d94a37755eb78d537b4d102920
josephwere <josephogwe8@gmail.com>
2026-01-02 00:47:06 +0300

    Create productivity_agent.go

---
+++ kernel/agents/productivity_agent.go
@@ -0,0 +1,40 @@
+package agents
+
+import (
+	"fmt"
+	"neuroedge/kernel/core"
+)
+
+type ProductivityAgent struct {
+	EventBus *core.EventBus
+}
+
+func NewProductivityAgent(bus *core.EventBus) *ProductivityAgent {
+	return &ProductivityAgent{
+		EventBus: bus,
+	}
+}
+
+func (p *ProductivityAgent) Start() {
+	fmt.Println("🚀 ProductivityAgent started")
+	ch := make(chan map[string]interface{})
+	p.EventBus.Subscribe("productivity:update", ch)
+	go func() {
+		for event := range ch {
+			fmt.Println("[ProductivityAgent] Productivity Event:", event)
+			p.OptimizeTasks(event)
+		}
+	}()
+}
+
+func (p *ProductivityAgent) Stop() {
+	fmt.Println("🛑 ProductivityAgent stopped")
+}
+
+func (p *ProductivityAgent) Name() string {
+	return "ProductivityAgent"
+}
+
+func (p *ProductivityAgent) OptimizeTasks(data map[string]interface{}) {
+	fmt.Println("[ProductivityAgent] Task optimization:", data)
+}
