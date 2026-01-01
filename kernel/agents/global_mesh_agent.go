d03ee3ccd086c506ac2508cab23e4b4b5bf4a82d
josephwere <josephogwe8@gmail.com>
2026-01-01 23:54:35 +0300

    Update global_mesh_agent.go

--- kernel/agents/global_mesh_agent.go
+++ kernel/agents/global_mesh_agent.go
@@ -1,41 +1,28 @@
 package agents
 
 import (
-    "fmt"
-    "neuroedge/kernel/events"
+	"fmt"
+	"neuroedge/kernel/core"
 )
 
 type GlobalMeshAgent struct {
-    EventBus *events.EventBus
-    NameStr  string
+	EventBus *core.EventBus
 }
 
-func NewGlobalMeshAgent(bus *events.EventBus) *GlobalMeshAgent {
-    agent := &GlobalMeshAgent{
-        EventBus: bus,
-        NameStr:  "GlobalMeshAgent",
-    }
-    // Subscribe to example events
-    ch := make(chan map[string]interface{})
-    bus.Subscribe("mesh:update", ch)
-    go agent.Listen(ch)
-    return agent
-}
-
-func (g *GlobalMeshAgent) Listen(ch chan map[string]interface{}) {
-    for data := range ch {
-        fmt.Println("🌐 GlobalMeshAgent received event:", data)
-    }
+func NewGlobalMeshAgent(bus *core.EventBus) *GlobalMeshAgent {
+	return &GlobalMeshAgent{
+		EventBus: bus,
+	}
 }
 
 func (g *GlobalMeshAgent) Start() {
-    fmt.Println("🚀 GlobalMeshAgent started")
+	fmt.Println("🚀 GlobalMeshAgent started")
 }
 
 func (g *GlobalMeshAgent) Stop() {
-    fmt.Println("🛑 GlobalMeshAgent stopped")
+	fmt.Println("🛑 GlobalMeshAgent stopped")
 }
 
 func (g *GlobalMeshAgent) Name() string {
-    return g.NameStr
+	return "GlobalMeshAgent"
 }
