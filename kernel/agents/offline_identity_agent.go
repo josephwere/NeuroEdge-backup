1294dcd7dd4296df6fcd46407597fabd1372cd7c
josephwere <josephogwe8@gmail.com>
2026-01-02 00:38:57 +0300

    Create offline_identity_agent.go

---
+++ kernel/agents/offline_identity_agent.go
@@ -0,0 +1,40 @@
+package agents
+
+import (
+	"fmt"
+	"neuroedge/kernel/core"
+)
+
+type OfflineIdentityAgent struct {
+	EventBus *core.EventBus
+}
+
+func NewOfflineIdentityAgent(bus *core.EventBus) *OfflineIdentityAgent {
+	return &OfflineIdentityAgent{
+		EventBus: bus,
+	}
+}
+
+func (o *OfflineIdentityAgent) Start() {
+	fmt.Println("🚀 OfflineIdentityAgent started")
+	ch := make(chan map[string]interface{})
+	o.EventBus.Subscribe("identity:offline:update", ch)
+	go func() {
+		for event := range ch {
+			fmt.Println("[OfflineIdentityAgent] Offline Identity Event:", event)
+			o.ValidateOffline(event)
+		}
+	}()
+}
+
+func (o *OfflineIdentityAgent) Stop() {
+	fmt.Println("🛑 OfflineIdentityAgent stopped")
+}
+
+func (o *OfflineIdentityAgent) Name() string {
+	return "OfflineIdentityAgent"
+}
+
+func (o *OfflineIdentityAgent) ValidateOffline(data map[string]interface{}) {
+	fmt.Println("[OfflineIdentityAgent] Offline identity verified:", data)
+}
