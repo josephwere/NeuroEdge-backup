9f4b3201a64eb1645765a87f04628fe07722f745
josephwere <josephogwe8@gmail.com>
2026-01-02 08:37:44 +0300

    Create ledger.go

---
+++ kernel/economy/ledger/ledger.go
@@ -0,0 +1,15 @@
+package ledger
+
+import "kernel/economy/wallet"
+
+type Ledger struct {
+	Transactions []wallet.Transaction
+}
+
+func NewLedger() *Ledger {
+	return &Ledger{Transactions: []wallet.Transaction{}}
+}
+
+func (l *Ledger) Record(tx wallet.Transaction) {
+	l.Transactions = append(l.Transactions, tx)
+}
