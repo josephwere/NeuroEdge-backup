2121f67c88b451bacd733828f18233c41d329045
josephwere <josephogwe8@gmail.com>
2026-01-04 15:11:48 +0300

    Create stream.ts

---
+++ orchestrator/utils/stream.ts
@@ -0,0 +1,16 @@
+import { EventBus } from "../core/event_bus";
+
+export class StreamLogger {
+  constructor(private bus: EventBus) {}
+
+  start() {
+    this.bus.subscribe("dev:result", (res: any) => {
+      console.log(`📡 [Stream] ${res.success ? "Success" : "Fail"} Output:\n${res.stdout}`);
+      if (res.stderr) console.error(`⚠️ [Stream Error] ${res.stderr}`);
+    });
+
+    this.bus.subscribe("remote:execution:received", (req: any) => {
+      console.log(`🌐 [Remote Node] Executing command: ${req.command}`);
+    });
+  }
+}
