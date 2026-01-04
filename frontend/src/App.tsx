f1f9dcdd8754b7253e7e83d9977f7e9a7bf3be05
josephwere <josephogwe8@gmail.com>
2026-01-05 02:42:10 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,10 @@
 import React from "react";
-import MainChat from "./components/MainChat/MainChat";
-import FloatingChat from "./components/FloatingChat/FloatingChat";
+import ReactDOM from "react-dom";
+import NeuroEdgeChat from "./components/NeuroEdgeChat";
+import { OrchestratorClient } from "./services/orchestrator_client";
 
-function App() {
-  return (
-    <div style={{ display: "flex", height: "100vh" }}>
-      {/* Main Chat takes majority of space */}
-      <div style={{ flex: 3, borderRight: "1px solid #ccc" }}>
-        <MainChat />
-      </div>
+const orchestrator = new OrchestratorClient();
 
-      {/* Floating Chat overlay/panel */}
-      <div style={{ flex: 1, position: "relative" }}>
-        <FloatingChat />
-      </div>
-    </div>
-  );
-}
+const App = () => <NeuroEdgeChat orchestrator={orchestrator} />;
 
-export default App;
+ReactDOM.render(<App />, document.getElementById("root"));
