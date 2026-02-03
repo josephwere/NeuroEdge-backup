065809d554549544a8f7e3a73cef6bef50246ac5
josephwere <josephogwe8@gmail.com>
2026-02-03 18:36:41 +0300

    Update MainChat.tsx

--- frontend/src/components/MainChat.tsx
+++ frontend/src/components/MainChat.tsx
@@ -5,9 +5,9 @@ import { OrchestratorClient } from "@/services/orchestrator_client";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { okaidia } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { saveToCache, getCache } from "@/services/offlineCache";
-import AISuggestionOverlay from "@/AISuggestionsOverlay";
+import AISuggestionOverlay from "@/components/AISuggestionsOverlay";
 import { generateSuggestions, AISuggestion } from "@/services/aiSuggestionEngine";
-import { FounderMessage } from "@/FounderAssistant";
+import { FounderMessage } from "@/components/FounderAssistant";
 
 interface Message {
   id: string;
