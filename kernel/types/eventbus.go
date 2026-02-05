e61dd2ab6d930f1b49ff83d04d67afc54354a800
josephwere <josephogwe8@gmail.com>
2026-02-05 21:53:23 +0300

    Delete kernel/types/eventbus.go

--- kernel/types/eventbus.go
+++
@@ -1,9 +0,0 @@
-// kernel/types/eventbus.go
-package types
-
-type Event map[string]interface{}
-
-type EventBus interface {
-	Subscribe(eventName string, ch chan Event)
-	Publish(eventName string, payload Event)
-}
