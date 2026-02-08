9e19d57d7f4dc26046bc8995d219dd319b8a49c1
josephwere <josephogwe8@gmail.com>
2026-02-08 23:52:45 +0300

    Create generated.go

---
+++ kernel/ml/orchestrator/generated/generated.go
@@ -0,0 +1,14 @@
+package generated
+
+// This is a stub package for orchestrator code generation
+
+type OrchestratorClient struct{}
+
+func NewOrchestratorClient() *OrchestratorClient {
+	return &OrchestratorClient{}
+}
+
+func (o *OrchestratorClient) ExecuteTask(task string, data map[string]interface{}) error {
+	// stub function
+	return nil
+}
