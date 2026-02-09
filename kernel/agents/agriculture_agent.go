08bb2779f953e6405bb7e713781ca164f9b8644a
josephwere <josephogwe8@gmail.com>
2026-02-09 17:00:53 +0300

    Update agriculture_agent.go

--- kernel/agents/agriculture_agent.go
+++ kernel/agents/agriculture_agent.go
@@ -19,10 +19,18 @@ func NewAgricultureAgent(bus *types.EventBus) *AgricultureAgent {
 func (a *AgricultureAgent) Start() {
 	fmt.Println("🚀 AgricultureAgent started")
 
-	// Use a subscriber function instead of a channel
+	// Subscribe directly with a function
 	a.EventBus.Subscribe("agriculture:update", func(event types.Event) {
-		fmt.Println("[AgricultureAgent] Agriculture Event:", event)
-		a.OptimizeFarm(event)
+		fmt.Println("[AgricultureAgent] Agriculture Event:", event.Data)
+
+		// Type assertion to map[string]interface{}
+		data, ok := event.Data.(map[string]interface{})
+		if !ok {
+			fmt.Println("[AgricultureAgent] Warning: event.Data is not a map[string]interface{}")
+			return
+		}
+
+		a.OptimizeFarm(data)
 	})
 }
 
@@ -34,6 +42,7 @@ func (a *AgricultureAgent) Name() string {
 	return "AgricultureAgent"
 }
 
-func (a *AgricultureAgent) OptimizeFarm(data types.Event) {
+// Optimized farm method now receives map[string]interface{}
+func (a *AgricultureAgent) OptimizeFarm(data map[string]interface{}) {
 	fmt.Println("[AgricultureAgent] Optimizing farm:", data)
 }
