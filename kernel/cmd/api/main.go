fb2e4c0d7ca2e919aef869c07b301aa32414153a
josephwere <josephogwe8@gmail.com>
2026-02-11 14:19:21 +0300

    Create main.go

---
+++ kernel/cmd/api/main.go
@@ -0,0 +1,46 @@
+//kernel/cmd/api/main.go
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	handlers "neuroedge/kernel/api"
+)
+
+func main() {
+	fmt.Println("Starting NeuroEdge API on :8080")
+
+	router := handlers.NewRouter()
+
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("API server error: %v", err)
+		}
+	}()
+
+	<-stop
+	fmt.Println("Shutting down API...")
+
+	shutdownCtx, cancel := time.WithTimeout(time.Now().Add(5*time.Second))
+	defer cancel()
+
+	_ = server.Shutdown(shutdownCtx)
+	fmt.Println("API stopped")
+}
